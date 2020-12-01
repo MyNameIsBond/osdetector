b63aed2f5036a6b265184329e29977b0b6e1a0d5
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-12-01 01:40:22 +0000

    Initial Commit

---
+++ Shared/OSDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSDetectionApp.swift
+//  Shared
+//
+//  Created by Tony Hajdini on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct OSDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
